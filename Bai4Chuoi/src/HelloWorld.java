5a6bbb1fee020f3acef463d30fe3ac344942028c
Đỗ Đăng Khôi <khoido.280205@gmail.com>
2026-01-27 22:47:33 +0700

    BAI1-BTH4-Code và chay các ví dụ HelloWord

---
+++ Bai4Chuoi/src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String HelloWorld = """
+                ======================
+                HELLO, MODERN JAVA!
+                ======================
+                by ĐỖ ĐĂNG KHÔI
+                ======================
+                """;
+        System.out.println(HelloWorld);
+    }
+}
