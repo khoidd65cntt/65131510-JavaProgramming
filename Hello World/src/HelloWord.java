e37cde54599b74fe3f26415e288a9df77542869a
Đỗ Đăng Khôi <khoido.280205@gmail.com>
2026-01-27 22:20:49 +0700

    Bài 1 BTTH 1 in hello world và thay đổi thông điệp

---
+++ Hello World/src/HelloWord.java
@@ -0,0 +1,8 @@
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Do Dang Khoi");
+        System.out.println("65131510");
+
+    }
+}
