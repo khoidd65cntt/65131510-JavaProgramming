f2dbb25d8e2bb265217ecaa2ec460916fcd4a939
Đỗ Đăng Khôi <khoido.280205@gmail.com>
2026-02-02 07:56:47 +0700

    Lab1_Bai2_ChuviDT

---
+++ Lab1_Bai2_ChuviDT/src/HinhChuNhat.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class HinhChuNhat {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhập chiều dài: ");
+        double dai = scanner.nextDouble();
+
+        System.out.print("Nhập chiều rộng: ");
+        double rong = scanner.nextDouble();
+
+        double chuVi = (dai + rong) * 2;
+        double dienTich = dai * rong;
+        double canhNho = Math.min(dai, rong);
+
+        System.out.println("Chu vi: " + chuVi);
+        System.out.println("Diện tích: " + dienTich);
+        System.out.println("Cạnh nhỏ: " + canhNho);
+
+        scanner.close();
+    }
+}
